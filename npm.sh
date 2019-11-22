3e878110f6ec0897124c5e22746b4aa8a762e796
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-11-22 10:53:25 +0000

    Removed bash file

--- npm.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-VERSION=$(node -p "require('./package.json').version")
-PUBLISHED_VERSION=$(npm view @ministryofjustice/fb-editor-node version);
-
-if [ "$VERSION" != "$PUBLISHED_VERSION" ]
-then
-  # npm publish
-fi
