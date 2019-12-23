65c07ecc93f60c504a2790c577f83bb83a6e77c2
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-12-23 08:03:02 +0000

    `git add assets templates specifications`

--- build/gulp/transform/specifications.js
+++ build/gulp/transform/specifications.js
@@ -13,7 +13,7 @@ const STYLESHEETS = /(assets\/stylesheets\/.*-)\d+\.\d+\.\d+(.css)/g
 const getFileData = async (filePath) => readFile(filePath, 'utf8')
 const setFileData = async (filePath, fileData) => writeFile(filePath, fileData, 'utf8')
 
-const getFilePathList = () => new Promise((resolve, reject) => glob('./specifications/**/*/*.njk', (e, a) => !e ? resolve(a) : reject(e)))
+const getFilePathList = () => new Promise((resolve, reject) => glob(['./specifications/**/*/*.njk', './specifications/**/*/*.html'], (e, a) => !e ? resolve(a) : reject(e)))
 const setFilePathList = (filePathList) => Promise.all(filePathList.map(async (filePath) => setFileData(filePath, (await getFileData(filePath)).replace(JAVASCRIPTS, `$1${version}$2`).replace(STYLESHEETS, `$1${version}$2`))))
 
 async function transform () {
